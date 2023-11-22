2f58a762647d75353503380aed60e1faf3af3e75
toivoantero <125696622+toivoantero@users.noreply.github.com>
2023-11-22 13:03:35 +0200

    Vastausraporttisivu hakee jsonin ja renderöi sen

--- src/components/Raportti.jsx
+++ src/components/Raportti.jsx
@@ -1,14 +1,53 @@
-import { Box, Typography, Paper } from '@mui/material';
+import { Box, Typography, Paper, TextField } from '@mui/material';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function Raportti() {
+
+    const [data, setData] = useState({});
+    const [questions, setQuestions] = useState([]);
+    /*const [answers, setAnswers] = useState();*/
+
+    const fetchData = () => {
+
+        fetch('http://localhost:8080/queries/1')
+            .then(response => response.json())
+            .then(responseData => {
+                setData(responseData)
+                setQuestions(responseData.questions)
+                console.log(questions)
+            })
+            .catch(err => console.error(err))
+    }
+
+    useEffect(fetchData, []);
+
     return (
         <Box
             width='80vw'
             marginX="auto"
             sx={{
-                textAlign: 'center',
+                textAlign: 'left',
             }}>
-            <Typography variant='h5'>Tulosraportti</Typography>
+            {console.log(questions)}
+            <Typography margin={1} color='primary' variant='h5'>{data.title}</Typography>
+            <Typography marginBottom={4} sx={{ fontSize: '19px' }}>{data.description}</Typography>
+            
+            {questions.map((question, questionIndex) => (
+                <Box key={question.id}>
+                    <p>
+                        {questionIndex + 1}. {question.questionText}<br />
+                    </p>
+                    {question.answers.map((answer, answerIndex) => (
+                        <ul key={answer.id}>
+                            <li>
+                                {answer.answerText}<br />
+                            </li>
+                        </ul>
+                    ))}
+                </Box>
+            ))}
+
         </Box >
     );
 }
